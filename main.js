190b7fd2264af23082cc4f7d8a015d0a0037295b
FEPSFY6816664 <146086821+FEPSFY6816664@users.noreply.github.com>
2023-12-02 16:07:57 +0000

    images added

---
+++ main.js
@@ -0,0 +1,25 @@
+var slideimages = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.jpg", "slide5.jpg"];
+var currentSlide = 0;
+
+function showSlide(){
+    document.getElementById('slide').src = "assets/"+slideimages[currentSlide];
+}
+
+
+function nextSlide(){
+    currentSlide = (currentSlide + 1) % slideimages.length;
+    showSlide()
+}
+
+function prevSlide(){
+    currentSlide = (currentSlide - 1 + slideimages.length) % slideimages.length;
+    showSlide()
+}
+
+
+function showSlideN(slideNumber){
+    currentSlide = slideNumber - 1;
+    showSlide();
+}
+
+showSlide();
